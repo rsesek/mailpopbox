6115c0966413b1b3b0c896a1f6700a7ded1243b3
Robert Sesek <rsesek@bluestatic.org>
2020-05-23 15:25:24 -0400

    Add license information.

--- smtp/conn_test.go
+++ smtp/conn_test.go
@@ -1,3 +1,9 @@
+// mailpopbox
+// Copyright 2020 Blue Static <https://www.bluestatic.org>
+// This program is free software licensed under the GNU General Public License,
+// version 3.0. The full text of the license can be found in LICENSE.txt.
+// SPDX-License-Identifier: GPL-3.0-only
+
 package smtp
 
 import (
